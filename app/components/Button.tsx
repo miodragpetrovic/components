9c3508dcefe9bdebca3649b8e78400ed60af71cd
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2024-11-21 19:24:36 +0100

    new

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Button = () => {
   return (
     <div>
-        <Link href='/' className='text-white border border-[#6c757d] rounded-3xl hover:bg-[#6c757d] px-5 py-2'>
+        <Link href='/' className='text-white border border-[#6c757d] rounded-3xl hover:bg-[#495057] hover:border-[#495057] px-5 py-2'>
             Sign Up
         </Link>
     </div>
