66085b637b59756057786cfa43f600fff13ab67c
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2024-11-21 18:36:32 +0100

    componnents button

---
+++ app/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Button = () => {
+  return (
+    <div>
+        <Link href='/' className='text-white'>
+            Login
+        </Link>
+    </div>
+  )
+}
+
+export default Button
